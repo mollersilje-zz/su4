d98b472fca92a52b531091120f7b5da7f7e3e834
maltel <maltel@stud.ntnu.no>
2013-03-11 11:23:14 +0100

    Create Employee.java

---
+++ su4/src/fellesp/Employee.java
@@ -0,0 +1,28 @@
+package FellesP;
+
+public class Employee {
+  
+	private String username;
+	private String password;
+	
+	public Employee(String username) {
+		this.username = username;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
