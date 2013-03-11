6c0367d910d05bc9e64282d38ebade7f2a00124b
maltel <maltel@stud.ntnu.no>
2013-03-11 11:23:43 +0100

    Create Appointment.java

---
+++ su4/src/fellesp/Appointment.java
@@ -0,0 +1,21 @@
+package FellesP;
+
+import java.util.ArrayList;
+
+public class Appointment {
+  
+	
+	private String date;
+	private int startTime;
+	private int endTime;
+	private Room room;
+	private String des;
+	private boolean type;
+	private Employee owner;
+	ArrayList<Employee> participants;
+	
+	
+	
+	
+
+}
