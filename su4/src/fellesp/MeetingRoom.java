8bb973c4dcb1db5316530afef2f697371baa031a
simenva <aakhus.simen@gmail.com>
2013-03-12 10:46:39 +0100

    Update MeetingRoom.java

--- su4/src/fellesp/MeetingRoom.java
+++ su4/src/fellesp/MeetingRoom.java
@@ -1,10 +1,19 @@
 package fellesp;
 
 public class MeetingRoom {
-	private int aa = 0;
-	private String paa = "Hello";
-	YOOOOOOOOOOOOOOOO
+	private int RoomNumber;
+	private int capacity;
 	
-	pp
-	edit
+	public MeetingRoom(int Roomnumber, int capacity){
+		this.Roomnumber=Roomnumber;
+		this.capacity=capacity;
+	}
+	
+	public int getRoomNumber(){
+		return this.RoomNumber;
+	}
+	
+	public int getCapacity(){
+		return this.capacity;
+	}
 }
