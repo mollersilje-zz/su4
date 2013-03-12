7dd9af0e1c761cd65c922cdb759980cb9d5dff71
mollersilje <mollersilje@gmail.com>
2013-03-12 10:48:38 +0100

    Create Alarm.java

---
+++ su4/src/fellesp/Alarm.java
@@ -0,0 +1,22 @@
+package Calendar;
+
+import java.sql.Time;
+
+public class Alarm {
+  
+	private Time alarmtime;
+	
+	public Alarm(Time alarmtime){
+		
+		
+	}
+
+	public Time getAlarmtime() {
+		return alarmtime;
+	}
+
+	public void setAlarmtime(Time alarmtime) {
+		this.alarmtime = alarmtime;
+	}
+	
+}
