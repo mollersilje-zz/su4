66eab530e41ab340d9e41280c06f9f4d61629a38
mollersilje <mollersilje@gmail.com>
2013-03-12 11:50:58 +0100

    Update Notification.java

--- su4/src/fellesp/Notification.java
+++ su4/src/fellesp/Notification.java
@@ -5,9 +5,10 @@ import java.sql.Time;
 public class Notification {
 	
 	private Time alarm;
+	private int i;
 
 	public Notification() {
-		
+		this.i
 	}
 	public Time getAlarm() {
 		return alarm;
@@ -15,6 +16,10 @@ public class Notification {
 
 	public void setAlarm(Time alarm) {
 		this.alarm = alarm;
+	}
+	
+	public int response(int i){
+		if (i = )
 	}
 	
 	public boolean acceptInvite(){
