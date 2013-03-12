da25eaaff9b873cdb81df02203dd92a28f77dfe5
simenva <aakhus.simen@gmail.com>
2013-03-12 11:19:13 +0100

    Update CalandarHandler.java

--- su4/src/fellesp/CalandarHandler.java
+++ su4/src/fellesp/CalandarHandler.java
@@ -1,2 +1,14 @@
-
 package fellesp;
+
+public class CalandarHandler{
+   
+   private Employee employee = new Employee(username, password);
+   
+   public boolean LogIn(String username, String password1){
+     //SPØRRING:password2 =select password from Employee as e where username=e.username
+     if password1=password2{
+       return true;
+     }
+   }
+   
+}
