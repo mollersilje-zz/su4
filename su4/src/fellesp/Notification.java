caecfd566e02a2e972ef0fc1d11ba14d598fbc5e
mollersilje <mollersilje@gmail.com>
2013-03-12 10:52:57 +0100

    Update Notification.java

--- su4/src/fellesp/Notification.java
+++ su4/src/fellesp/Notification.java
@@ -1,5 +1,18 @@
 package Calendar;
 
+import java.sql.Time;
+
 public class Notification {
+  
+	private Time alarm;
+
+	public Time getAlarm() {
+		return alarm;
+	}
+
+	public void setAlarm(Time alarm) {
+		this.alarm = alarm;
+	}
+	
 
 }
