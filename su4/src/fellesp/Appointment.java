3f0d262f18babedd2c36bf506d4fdad01b5b810f
Eva <Eva@78.91.51.18>
2013-03-12 13:10:53 +0100

    Appointment

--- su4/src/fellesp/Appointment.java
+++ su4/src/fellesp/Appointment.java
@@ -1,21 +1,54 @@
-package fellesp;
+package prosjekt;
 
 import java.util.ArrayList;
+import java.sql.Date;
+import java.sql.Time;
 
 public class Appointment {
   
-	
-	private String date;
-	private int startTime;
-	private int endTime;
+
+	private Date date;
+	private Time startTime;
+	private Time endTime;
 	private MeetingRoom room;
 	private String des;
 	private boolean type;
 	private Employee owner;
 	ArrayList<Employee> participants;
+
+	public Appointment(Date date, Time startTime, Time endTime, String des, boolean type, Employee owner){
+		this.date = date;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.des = des;
+		this.type = type;
+	}
 	
+	public Date getDate(){
+		return date;
+	}
+	public Time getStartTime(){
+		return startTime;
+	}
+	public Time getEndTime(){
+		return endTime;
+	}
+	public Arraylist<MeetingRoom> ReserveRoom(date, startTime, endTime, type){
+		rooms = new ArrayList<MeetingRoom>();
+		for (int i = 0;i < rooms.size(); i++){
+			if (i.date != date){
+				return rooms;
+			}else{
+				if((i.startTime > startTime) && (i.endTime <= endTime))
+			}
+		}
+	}
 	
-	
-	
+	public String getDes(){
+		return des;
+	}
+	public boolean getType(){
+		return type;
+	}
 
 }
