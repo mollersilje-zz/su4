4926d8907a688ee76c36686741d897424c1533d2
simenva <aakhus.simen@gmail.com>
2013-03-12 11:19:57 +0100

    Update Employee.java

--- su4/src/fellesp/Employee.java
+++ su4/src/fellesp/Employee.java
@@ -5,8 +5,9 @@ public class Employee {
 	private String username;
 	private String password;
 	
-	public Employee(String username) {
+	public Employee(String username, String password) {
 		this.username = username;
+		this.password = password;
 	}
 	
 	public String getUsername() {
