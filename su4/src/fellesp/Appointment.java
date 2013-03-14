42abefececf34feda1f02b8bf8011fd58f57fd2a
Sivert Johan Solem <sivert.solem@gmail.com>
2013-03-14 14:51:42 +0100

    ¨Conflict Resolution

--- su4/src/fellesp/Appointment.java
+++ su4/src/fellesp/Appointment.java
@@ -1,77 +1,4 @@
-<<<<<<< HEAD
-package fellesp;
 
-import java.util.ArrayList;
-import java.sql.Date;
-import java.sql.Time;
-
-public class Appointment {
-  
-	private int id;
-	private Date date;
-	private Time startTime;
-	private Time endTime;
-	private String place;
-	private String description;
-	private String owner;
-	private int type; // "1" avtale og "2" er møte
-
-
-	public Appointment(int id, Date date, Time startTime, Time endTime, String place, String description, String owner, int type){
-		this.id = id;
-		this.date = date;
-		this.startTime = startTime;
-		this.endTime = endTime;
-		this.place = place;
-		this.description = description;
-		this.owner = owner;
-		this.type = type;
-	}
-
-	public int getId(){
-		return id;
-	}
-	public Date getDate(){
-		return date;
-	}
-	public Time getStartTime(){
-		return startTime;
-	}
-	public Time getEndTime(){
-		return endTime;
-	}
-	
-	public String getPlace(){
-		return place;
-	}
-	
-	public String getDescription(){
-		return description;
-	}
-
-	public String getOwner(){
-		return owner;
-	}
-	
-	public int getType(){
-		return type;
-	}
-	
-	public ArrayList<MeetingRoom> ReserveRoom(Date date, Time startTime, Time endTime, boolean type, int participants){
-		ArrayList<MeetingRoom> rooms = new ArrayList<MeetingRoom>();//Henter rom fra database hvor capacity >= participants
-		ArrayList<MeetingRoom> availableRooms = new ArrayList<MeetingRoom>();
-
-		for (int i = 0;i < rooms.size(); i++){
-			if (rooms.get(i).isAvailable(date, startTime, endTime)){
-				availableRooms.add(rooms.get(i));
-			}
-		}
-	return availableRooms;
-	}
-
-
-}
-=======
 package fellesp;
 
 import java.util.ArrayList;
@@ -143,4 +70,3 @@ public class Appointment {
 
 
 }
->>>>>>> AppointmentTest
--- su4/src/fellesp/AppointmentFactory.java
+++ su4/src/fellesp/AppointmentFactory.java
@@ -8,77 +8,45 @@ import java.util.Properties;
 
 public class AppointmentFactory {
 	
-<<<<<<< HEAD
+
 	private static DBConnection db;
-=======
-	static DBConnection db;
->>>>>>> AppointmentTest
+
 	
 	public AppointmentFactory(Properties properties) throws ClassNotFoundException, SQLException
 	{
 		 db = new DBConnection(properties);
 	}
 	
-<<<<<<< HEAD
-	public static Appointment createAppointment(Date date, Time startTime, Time endTime, String place, String description, int type, String owner) throws ClassNotFoundException, SQLException{
-=======
+
 	public static Appointment createAppointment(Date date, Time startTime, Time endTime, String description, boolean meeting, String owner) throws ClassNotFoundException, SQLException{
->>>>>>> AppointmentTest
+
 		java.sql.Date sqlDate = new java.sql.Date(date.getTime());
 		java.sql.Time sqlStartTime = new java.sql.Time(startTime.getTime());
 		java.sql.Time sqlEndTime = new java.sql.Time(endTime.getTime());
 		
 		int id = getNextId();
 		
-<<<<<<< HEAD
-		Appointment a= new Appointment(id, date, startTime, endTime, place, description, owner, type);
-		String query=String.format("insert into appointment " + "(date, startTime, endTime, description, type, owner) values (" 
-				+ id + "," + sqlDate + "," + sqlStartTime +"," + sqlEndTime +"," + description + "," 
-				+ type + "," + owner + ");" );
-	
-=======
+
 		Appointment a= new Appointment(id, date, startTime, endTime, description, owner, meeting);
 		String query=String.format("insert into appointment " + "(date, startTime, endTime, description, type, owner) values (" + id + "," + sqlDate + "," + sqlStartTime +"," + sqlEndTime +"," + description + "," 
 		+ meeting + "," + owner + ");" );
 		
->>>>>>> AppointmentTest
+
 		db.initialize();
 		db.makeSingleUpdate(query);
 		db.close();
 		return a;
 	}
 	
-<<<<<<< HEAD
+
 	public static Appointment getAppointment(int id) throws SQLException, ClassNotFoundException{
 		String query = String.format("select * from Appointmet where id =" + id + ";" );
-=======
-	public static  Appointment getAppointment(int id) throws SQLException, ClassNotFoundException{
-		String query = String.format("select * from Appointmet where id =" + id);
->>>>>>> AppointmentTest
+
 		db.initialize();
 		ResultSet rs = db.makeSingleQuery(query);
 		Date date = null;
 		Time startTime = null;
 		Time endTime = null;
-<<<<<<< HEAD
-		String place = null;
-		String description = null;
-		int type = 1; // "1" avtale og "2" er møte
-		String owner = null;
-		
-		while (rs.next()){
-			date = rs.getDate(2);
-			startTime = rs.getTime(3);
-			endTime = rs.getTime(4);
-			place = rs.getString(5);
-			description = rs.getString(6);
-			type = rs.getInt(7);
-			owner = rs.getString(8);
-		}
-		
-		Appointment a= new Appointment(id, date, startTime, endTime, place, description, owner, type);
-
-=======
 		String description = null;
 		boolean meeting = true; // "1" avtale og "2" er møte
 		String owner = null;
@@ -93,19 +61,14 @@ public class AppointmentFactory {
 		}
 		
 		Appointment a= new Appointment(id, date, startTime, endTime, description, owner, meeting);
->>>>>>> AppointmentTest
 		db.close();
 		
 		return a;
 	}
 	
-<<<<<<< HEAD
 	public static int getNextId() throws ClassNotFoundException, SQLException{
 		String query= "select max(appointmentID) from Appointment ;";
-=======
-	private static int getNextId() throws ClassNotFoundException, SQLException{
-		String query= "select max(appointmentID) from Appointment";
->>>>>>> AppointmentTest
+
 		db.initialize();
 		ResultSet rs = db.makeSingleQuery(query);
 		int nextId = 0;
@@ -120,11 +83,9 @@ public class AppointmentFactory {
 	
 	public static void deleteAppointment(String owner) throws ClassNotFoundException, SQLException{
 		
-<<<<<<< HEAD
+
 		String query =String.format("delete from Appintment where owner =" + owner + ";");
-=======
-		String query =String.format("delete from Appintment where owner =" + owner);
->>>>>>> AppointmentTest
+
 		db.initialize();
 		ResultSet rs = db.makeSingleQuery(query);
 		rs.close();
@@ -132,7 +93,7 @@ public class AppointmentFactory {
 		
 	}
 	
-<<<<<<< HEAD
+
 	public static void updateAppointment(String id){
 		String query = String.format("UPDATE User");
 		
@@ -146,7 +107,4 @@ public class AppointmentFactory {
 		
 	}
 }
-=======
-	
-}
->>>>>>> AppointmentTest
+
